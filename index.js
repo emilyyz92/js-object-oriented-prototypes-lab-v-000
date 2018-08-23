873d786c3fa53b4adba64df6c78b2af6fdbcb669
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-23 14:00:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ BoardMember.prototype.approve = function(){
     return 'You can do that!'
 }
 
-//
 BoardMember.prototype.doCharity = function(){
   return "I like to help people."
 }
 //
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
 //
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
